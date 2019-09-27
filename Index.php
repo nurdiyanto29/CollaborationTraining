7079aaeb6263df7d3fc03e4f20faf47e35f69cbb
hudaimi <55267457+hudaimi@users.noreply.github.com>
2019-09-27 10:49:17 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+echo "<h3>Hudaimi (175410083)</h3>"
+echo "<h3>Mahasiswa1</h3>"
+?>
